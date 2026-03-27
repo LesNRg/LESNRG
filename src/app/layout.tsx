924dd20b7a2c7cc77a85a1acefc081dc10d7b37b
Neil Goldman <neilgoldman@LESs-MacBook-Pro.local>
2026-03-27 06:40:38 -0400

    Set theme-color #111111 to make iPhone notch/status bar area black

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -52,6 +52,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(localBusinessSchema) }}
         />
         <link rel="manifest" href="/manifest.json" />
+        <meta name="theme-color" content="#111111" />
         <meta name="apple-mobile-web-app-capable" content="yes" />
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent" />
         <meta name="mobile-web-app-capable" content="yes" />
